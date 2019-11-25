d1af75f4399ac7c377760137aeb2c6aa62ccb7bb
HIT_kobs <Kjobs@users.noreply.github.com>
2019-11-25 15:17:33 +0800

    Create 037-Sudoku Solver.java

---
+++ LeetCodeOJ/Algorithms/037-Sudoku Solver.java
@@ -0,0 +1,36 @@
+class Solution {
+    public void solveSudoku(char[][] board) {
+        helper(board);
+    }
+
+    private boolean helper(char[][] board) {
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (board[i][j] == '.') {
+                    for (char k = '1'; k <= '9'; k++) {
+                        if (isValid(board, i, j, k)) {
+                            board[i][j] = k;
+                            if (helper(board)) return true;
+                            board[i][j] = '.';
+                        }
+                    }
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private boolean isValid(char[][] board, int row, int col, char ch) {
+        int rowOffset = row / 3 * 3;
+        int colOffset = col / 3 * 3;
+        for (int i = 0; i < 9; i++) {
+            if (board[row][i] == ch) return false;
+            if (board[i][col] == ch) return false;
+            if (board[rowOffset + i / 3][colOffset + i % 3] == ch) return false;
+        }
+        return true;
+    }
+}
+
+/* 回溯法求解 */
