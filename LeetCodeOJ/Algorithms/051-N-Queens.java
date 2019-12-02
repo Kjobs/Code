7ef1e0e24ff57be4cc1a59b513d2641272ab1382
HIT_kobs <Kjobs@users.noreply.github.com>
2019-12-02 10:04:06 +0800

    Create 051-N-Queens.java

---
+++ LeetCodeOJ/Algorithms/051-N-Queens.java
@@ -0,0 +1,59 @@
+class Solution {
+    public List<List<String>> solveNQueens(int n) {
+        List<List<String>> result = new ArrayList<>();
+        char[][] board = new char[n][n];
+        initBoard(board);
+        helper(result, board, 0);
+        return result;
+    }
+
+    private void initBoard(char[][] board) {
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board.length; j++) {
+                board[i][j] = '.';
+            }
+        }
+    }
+
+    private void helper(List<List<String>> res, char[][] board, int row) {
+        if (row == board.length) {
+            res.add(generateBoard(board));
+            return;
+        }
+        for (int col = 0; col < board.length; col++) {
+            if (isValid(board, row, col)) {
+                board[row][col] = 'Q';
+                helper(res, board, row + 1);
+                board[row][col] = '.';
+            }
+        }
+    }
+
+    private boolean isValid(char[][] board, int row, int col) {
+        for (int i = 0; i < row; i++) {
+            if (board[i][col] == 'Q') return false;
+        }
+        for (int j = 0; j < col; j++) {
+            if (board[row][j] == 'Q') return false;
+        }
+        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
+            if (board[i][j] == 'Q') return false;
+        }
+        for (int i = row - 1, j = col + 1; i >= 0 && j < board.length; i--, j++) {
+            if (board[i][j] == 'Q') return false;
+        }
+        return true;
+    }
+
+    private List<String> generateBoard(char[][] board) {
+        List<String> res = new ArrayList<>();
+        for (int i = 0; i < board.length; i++) {
+            StringBuilder sb = new StringBuilder();
+            for (int j = 0; j < board.length; j++) {
+                sb.append(board[i][j]);
+            }
+            res.add(sb.toString());
+        }
+        return res;
+    }
+}
