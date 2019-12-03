1cd183982592d66f0bde6590b7985bfb3d4619be
HIT_kobs <Kjobs@users.noreply.github.com>
2019-12-03 14:48:37 +0800

    Update 041-First Missing Positive.java

--- LeetCodeOJ/Algorithms/041-First Missing Positive.java
+++ LeetCodeOJ/Algorithms/041-First Missing Positive.java
@@ -2,7 +2,9 @@ class Solution {
     public int firstMissingPositive(int[] nums) {
         for (int i = 0; i < nums.length; i++) {
             while (nums[i] != i + 1) {
+                // 对于大于数组长度的数不用排序
                 if (nums[i] <= 0 || nums[i] > nums.length) break;
+                // 防止出现重复的数
                 if (nums[i] == nums[nums[i] - 1]) break;
                 numSwap(nums, i, nums[i] - 1);
             }
@@ -21,3 +23,5 @@ class Solution {
         nums[y] = temp;
     }
 }
+
+/* 基于桶排序实现基于O(n)的数组排序，然后找出于下标不一致的数（这里下标需+1） */
