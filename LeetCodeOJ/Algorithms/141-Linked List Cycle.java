a61f9548f62fbf1dfb46b43f4077920ed00501e9
HIT_kobs <Kjobs@users.noreply.github.com>
2020-01-10 16:33:27 +0800

    Create 141-Linked List Cycle.java

---
+++ LeetCodeOJ/Algorithms/141-Linked List Cycle.java
@@ -0,0 +1,14 @@
+/* 循环节点的问题，依旧采用双指针可以较有效解决 */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        if (head == null) return false;
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) return true;
+        }
+        return false;
+    }
+}
