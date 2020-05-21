a25afd9272887a8db4327587c75161ee50be6bce
HIT_kobs <Kjobs@users.noreply.github.com>
2020-05-21 22:24:03 +0800

    只出现一次的数字 II
    题目描述：给定一个非空整数数组，除了某个元素只出现一次以外，其余每个元素均出现了三次。找出那个只出现了一次的元素。

---
+++ LeetCodeOJ/Algorithms/137-SingleNumber.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        int once = 0;
+        int twice = 0;
+        for (int num : nums) {
+            once = ~twice & (once ^ num);
+            twice = ~once & (twice ^ num);
+        }
+        return once;
+    }
+}
